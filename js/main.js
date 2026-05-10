39d39e306788013b8e9e52db2f6ea224ec5c90f2
Tomas Alvarez Chavez <tomasgac.qa@gmail.com>
2026-05-10 11:44:12 -0400

    CHORE  | Agregar referencias iniciales del formulario y vinculación al html

---
+++ js/main.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const formulario =
+        document.getElementById("formularioInformacion");
+
+    const rut =
+        document.getElementById("rut");
+
+    const diplomado =
+        document.getElementById("diplomado");
+
+    const pais =
+        document.getElementById("pais");
+
+    const contacto =
+        document.getElementById("contacto");
+
+    const mensajeRut =
+        document.getElementById("mensajeRut");
+
+    const mensajeFormulario =
+        document.getElementById("mensajeFormulario");
+
+});
