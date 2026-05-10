d4d09c6bdfbd58640fd64cecdcdba8cddf0678eb
Tomas Alvarez Chavez <tomasgac.qa@gmail.com>
2026-05-10 13:53:56 -0400

    FEAT | Validación de formato de número telefónico

--- js/main.js
+++ js/main.js
@@ -23,11 +23,27 @@ document.addEventListener("DOMContentLoaded", () => {
         
     formulario.addEventListener("submit", (event) => {
         event.preventDefault();
-        
+
         const nombreDiplomado =
             diplomado.options[diplomado.selectedIndex].text;
 
         mensajeFormulario.textContent =
             `Gracias por solicitar más información sobre el ${nombreDiplomado}. Te contactaremos a la brevedad.`;
+    });
+    formulario.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        if (!contacto.value.startsWith("+56")) {
+            mensajeFormulario.textContent =
+                "El número debe comenzar con +56.";
+            return;
+        }
+
+        if (contacto.value.length !== 12) {
+            mensajeFormulario.textContent =
+                "El teléfono debe tener 12 caracteres.";
+            return;
+        }
+
     });
 });
