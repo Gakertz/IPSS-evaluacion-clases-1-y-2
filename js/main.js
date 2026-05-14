f8263f2b282cfd7c276ccad2fd0ebd15727b9722
sergiocasapia <sergio.casapia@gmail.com>
2026-05-13 21:19:37 -0400

    Feature scripts catalogos (#16)
    * fea: incorpora js botones descarga e informacion
    
    * feature : se incorpora js entrega informacion malla y informacion
    
    * feature : se incorpora funcional js a botones de descarg y envio informacion
    
    ---------
    
    Co-authored-by: Tomas Alvarez Chavez <93352642+Gakertz@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -75,4 +75,21 @@ document.addEventListener("DOMContentLoaded", () => {
             mensajeRut.style.color = "green";
         }
     });
+    const botonesDescargar = document.querySelectorAll(".btnDescargar");
+ 
+    botonesDescargar.forEach((boton) => {
+        boton.addEventListener("click", (event) => {
+            event.preventDefault();
+ 
+            alert("¡Iniciando la descarga de la malla!");
+        });
+    });
+
+    const botonesInfo = document.querySelectorAll(".btnInfo");
+        botonesInfo.forEach((boton) => {
+        boton.addEventListener("click", (event) => {
+            event.preventDefault();
+            alert("Cargando más información...");
+        });
+    });
 });
