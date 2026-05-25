7e0221c2b64f0f3263cf8a8f22d2c510a5fc4ad2
sergiocasapia <sergio.casapia@gmail.com>
2026-05-25 18:37:14 -0400

    feat: crear modulo base api.js para consumo de api

--- js/main.js
+++ js/main.js
@@ -92,12 +92,5 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 
-    const botonesInfo = document.querySelectorAll(".btnInfo");
 
-    botonesInfo.forEach((boton) => {
-        boton.addEventListener("click", (event) => {
-            event.preventDefault();
-            alert("Cargando más información...");
-        });
-    });
 });
